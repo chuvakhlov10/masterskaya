f241416a7a723d5fbeef390c30bcb13c573be308
chuvakhlov10 <chuvakhlov10@gmail.com>
2026-08-04 20:16:15 +0300

    Add secure Ably Realtime client options

---
+++ src/ably-secure-client.js
@@ -0,0 +1,63 @@
+import {
+  SECURE_ABLY_AUTH_ENDPOINT,
+  requestSecureAblyToken,
+} from "./ably-auth.js";
+
+const DEFAULT_RETRY_OPTIONS = Object.freeze({
+  disconnectedRetryTimeout: 2_000,
+  suspendedRetryTimeout: 5_000,
+  realtimeRequestTimeout: 15_000,
+  idlePeriod: 5_000,
+  heartbeatInterval: 5_000,
+});
+
+function makeError(code) {
+  const error = new Error(code);
+  error.code = code;
+  return error;
+}
+
+export function createSecureAblyAuthCallback({
+  clientId,
+  endpoint = SECURE_ABLY_AUTH_ENDPOINT,
+  fetchImpl = globalThis.fetch,
+  storage = globalThis.localStorage,
+} = {}) {
+  if (typeof clientId !== "string" || clientId.length < 8) {
+    throw makeError("CLIENT_ID_INVALID");
+  }
+
+  return (_tokenParams, callback) => {
+    if (typeof callback !== "function") return;
+
+    requestSecureAblyToken({
+      clientId,
+      endpoint,
+      fetchImpl,
+      storage,
+    }).then(
+      details => callback(null, details.token),
+      error => callback(error),
+    );
+  };
+}
+
+export function createSecureAblyRealtimeOptions({
+  clientId,
+  endpoint = SECURE_ABLY_AUTH_ENDPOINT,
+  fetchImpl = globalThis.fetch,
+  storage = globalThis.localStorage,
+  autoConnect = true,
+} = {}) {
+  return {
+    clientId,
+    autoConnect: autoConnect === true,
+    authCallback: createSecureAblyAuthCallback({
+      clientId,
+      endpoint,
+      fetchImpl,
+      storage,
+    }),
+    ...DEFAULT_RETRY_OPTIONS,
+  };
+}
