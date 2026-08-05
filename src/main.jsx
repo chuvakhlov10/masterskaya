102d353fa6fdc9309e292616dcf616b19f639a12
chuvakhlov10 <chuvakhlov10@gmail.com>
2026-08-05 11:20:24 +0300

    Install device pairing gate

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { ErrorBoundary } from './App.jsx'
+import DevicePairingGate from './DevicePairingGate.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <DevicePairingGate>
+        <App />
+      </DevicePairingGate>
     </ErrorBoundary>
   </React.StrictMode>
 )
